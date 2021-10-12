2bc0a9b35173abefc8c2da9374237b31190bb6ed
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2021-10-12 18:51:54 +0530

    Added more functionality to project.

--- pptr.js
+++ pptr.js
@@ -1,6 +1,14 @@
 let puppeteer = require("puppeteer");
 let firstTab;
-let browserOpenPromise = puppeteer.launch({ headless: false });
+let browserOpenPromise = puppeteer.launch(
+    {
+        headless: false,
+        defaultViewport: null,
+        slowMo: 200,
+        args: [
+            "--start-maximized"
+        ]
+    });
 browserOpenPromise.then(function (browserContextObj) {
     // console.log(("browser is launched!!"));
     let pagesPromise = browserContextObj.pages();
@@ -26,6 +34,30 @@ browserOpenPromise.then(function (browserContextObj) {
     }).then(function () {
         let click = firstTab.click("h3.LC20lb.DKV0Md");
         return click;
+    }).then(function () {
+        let wfs = firstTab.waitForSelector("input[type='text']", { visible: true });
+        return wfs;
+    }).then(function () {
+        let searchPromise = firstTab.type("input[type='text']", "apna college");
+        return searchPromise;
+    }).then(function () {
+        let wfs2 = firstTab.waitForSelector("button[id='search-icon-legacy']", { visible: true });
+        return wfs2;
+    }).then(function () {
+        let clickSearchOption = firstTab.click("button[id='search-icon-legacy']");
+        return clickSearchOption;
+    }).then(function () {
+        let wfs3 = firstTab.waitForSelector('a[title="String Builder | Java Placement Course Lecture 13" ]', { visible: true });
+        return wfs3;
+    }).then(function () {
+        let playVideo = firstTab.click('a[title="String Builder | Java Placement Course Lecture 13" ]');
+        return playVideo;
+    }).then(function () {
+        let wfs4 = firstTab.waitForSelector("button[class='ytp-fullscreen-button ytp-button']", { visible: true });
+        return wfs4;
+    }).then(function () {
+        let fullScreen = firstTab.click("button[class='ytp-fullscreen-button ytp-button']");
+        return fullScreen;
     }).catch(function (err) {
         console.log(err);
     })
